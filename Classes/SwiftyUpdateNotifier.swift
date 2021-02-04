b3d08fb0964fb0c4f0aa8eb50b1d3a9a706693e3
arjun.kt <arjun.kt@meridian.net.in>
2021-02-04 14:19:25 +0530

    #updated

--- Classes/SwiftyUpdateNotifier.swift
+++ Classes/SwiftyUpdateNotifier.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+import UIKit
+
+class SwiftyUpdateNotifier {
+    
+    func checkVersion(code: CountryCode, appId: String, window: UIWindow, versionLocal: String, appstoreUrl: String, updationTitle: String, updationMessage: String, callback: @escaping (Bool)->Void) {
+        VersionCheck.shared.isUpdateAvailable(code: code, appId: appId, window: window, versionLocal: versionLocal, appstoreUrl: appstoreUrl, updationTitle: updationTitle, updationMessage: updationMessage) { (val) in
+            callback(val)
+        }
+    }
+    
+}
